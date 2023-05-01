3060531f94b92ab76eb024397e7460fc6fd1a8f1
prataprohit526 <prataprohit526@gmail.com>
2023-05-02 00:07:20 +0530

    add all

--- src/components/counterContainer.js
+++ src/components/counterContainer.js
@@ -3,16 +3,19 @@ import Counter from "./counter";
 import { increment, decrement, reset } from "../actions";
 
 const mapStateToProps = (state) => {
-    return {
-        counter: state,
-    };
+  return {
+    counter: state,
+  };
 };
 const mapDispatchToProps = (dispatch) => {
-    return {
-        increment: () => dispatch(increment()),
-        decrement: () => dispatch(decrement()),
-        reset: () => dispatch(reset()),
-    };
+  return {
+    increment: () => dispatch(increment()),
+    decrement: () => dispatch(decrement()),
+    reset: () => dispatch(reset()),
+  };
 };
 
 // add code here
+const CounterContainer = connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+export default CounterContainer;
